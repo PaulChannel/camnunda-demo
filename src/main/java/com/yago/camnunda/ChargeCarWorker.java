dfd68f0342a2a5d0c3b9d023c177a626fe82e731
yougenhu <yougen.hu@ximalaya.com>
2023-04-25 23:20:33 +0800

    feat: 添加项目所需依赖

---
+++ src/main/java/com/yago/camnunda/ChargeCarWorker.java
@@ -0,0 +1,15 @@
+package com.yago.camnunda;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+/**
+ * @author: yougen.hu
+ * @time: 2023/4/25 22:27
+ */
+
+public class ChargeCarWorker {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ChargeCarWorker.class);
+
+}
